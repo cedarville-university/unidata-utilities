39c79bfdd65e4e0208cedf6ea57da0cf9669be6c
Nat Biggs <nbiggs112@cedarville.edu>
2020-01-13 14:17:11 -0500

    removes other utilities

--- unidata/cubak/view.spool/c_qclose.c
+++
@@ -1,8 +0,0 @@
-#include <stdio.h>
- 
-int c_qclose(q_file)
-long q_file;
-{
-   fclose((FILE *) q_file);
-   return(1);
-}
--- unidata/cubak/view.spool/qclose.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
- 
-int qclose(q_file)
-int q_file;
-{
-   return(close(q_file));
-}
